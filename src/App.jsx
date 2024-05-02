62cfa4b76d91613db1ca1ca132a2e9f1e542c204
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-05-02 18:45:42 -0300

    Clase 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import EjemploFunc from './Components/EjemploFunc'
+
+function App() {
+
+  return (
+    <>
+      <h1>Bienvenidos a React</h1>
+      <h4>Acá es donde vamos a empezar a trabajar</h4>
+      <EjemploFunc/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/EjemploFunc.jsx
@@ -0,0 +1,9 @@
+function EjemploFunc() {
+    let nombre = 'Carlos'
+    return <div>
+        <h1>Hola, {nombre}!</h1>
+        <h3>Este es mi primer componente</h3>
+    </div>
+}
+
+export default EjemploFunc
